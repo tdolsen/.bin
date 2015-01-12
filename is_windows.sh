15d67e6970f920534b8b114cdc0bda8c522f7424
Torkild Dyvik Olsen <torkild@tdolsen.net>
2015-01-12 14:50:03 +0100

    Added 'link' and 'rmlink', that supposedly should work on win32.

---
+++ is_windows.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Simple check to see if on Windows.
+is_windows() { [[ -n "$WINDIR" ]]; }
---
+++ rmlink.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source is_windows
+
+# Remove a link, cross-platform.
+rmlink() {
+    if is_windows; then
+        # Again, Windows needs to be told if it's a file or directory.
+        if [[ -d "$1" ]]; then
+            rmdir "$1";
+        else
+            rm "$1"
+        fi
+    else
+        rm "$1"
+    fi
+}
